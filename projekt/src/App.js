b05d5bfa13d2ef6d43b34afa4c16876ee96612ea
hopefulbastard <miloskousgaard@gmail.com>
2022-06-10 00:07:11 +0200

    .

--- projekt/src/App.js
+++ projekt/src/App.js
@@ -2,20 +2,23 @@ import "./App.css";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Frontpage from "./frontpage";
 import Settings from "./settings";
+import Archive from "./archive";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="app">
+        <nav>
+          <Link to="/">Back</Link>
+          <Link to="/settings">Settings</Link>
+          <Link to="/archive">Archive</Link>
+        </nav>
         <Routes>
           <Route path="/" element={<Frontpage />}></Route>
           <Route path="/settings" element={<Settings />}></Route>
+          <Route path="/archive" element={<Archive />}></Route>
         </Routes>
       </div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/settings">Settings</Link>
-      </nav>
     </BrowserRouter>
   );
 }
