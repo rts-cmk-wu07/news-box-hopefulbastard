548d69f71f55780d40b510243e6f154b62db0f10
hopefulbastard <miloskousgaard@gmail.com>
2022-06-21 00:54:44 +0200

    .

--- projekt/src/App.js
+++ projekt/src/App.js
@@ -3,11 +3,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Frontpage from "./frontpage";
 import Settings from "./settings";
 import Archive from "./archive";
+import { useSwipeable } from "react-swipeable";
 
 function App() {
+  const refreshswipe = useSwipeable({
+    onSwipedUp: () => window.location.reload(),
+  });
+
   return (
     <BrowserRouter>
-      <div className="app">
+      <div className="app" {...refreshswipe}>
         <Routes>
           <Route path="/" element={<Frontpage />}></Route>
           <Route path="/settings" element={<Settings />}></Route>
